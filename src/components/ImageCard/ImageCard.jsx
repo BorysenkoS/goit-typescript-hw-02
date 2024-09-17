5f61994a2c0b307f314a01742a96d3fcb0f07c59
Serhii <110692205+BorysenkoS@users.noreply.github.com>
2024-09-17 18:46:35 +0200

    Initial commit

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,21 @@
+import css from "./ImageCard.module.css";
+
+const ImageCard = ({ url, alt, openModal, setOnPhoto }) => {
+  const handleClick = () => {
+    setOnPhoto({ url: url.regular, alt: alt });
+    openModal();
+  };
+  return (
+    <div onClick={handleClick} className={css.wrapper}>
+      <img
+        className={css.photo}
+        src={url.small}
+        alt={alt}
+        width={320}
+        height={220}
+      />
+    </div>
+  );
+};
+
+export default ImageCard;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,54 @@
+import css from "./SearchBar.module.css";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const FeedbackSchema = Yup.object().shape({
+  searchTerm: Yup.string()
+    .min(2, "Too Short! Min 2 symbols.")
+    .max(50, "Too Long! Max 50 symbols.")
+    .required("Required! Enter any word..."),
+});
+
+const initialValues = {
+  searchTerm: "",
+};
+
+const SearchBar = ({ onSubmit }) => {
+  const handleSubmit = (values, actions) => {
+    onSubmit(values.searchTerm);
+    actions.resetForm();
+  };
+  return (
+    <div>
+      <header className={css.searchBar}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={FeedbackSchema}
+        >
+          <Form>
+            <label className={css.formLabel}>
+              <Field
+                className={css.searchInput}
+                type="text"
+                name="searchTerm"
+                placeholder="Search images and photos"
+              />
+            </label>
+            <button className={css.searchBtn} type="submit">
+              Search
+            </button>
+            <br />
+            <ErrorMessage
+              className={css.formErrorMessage}
+              name="searchTerm"
+              component="span"
+            />
+          </Form>
+        </Formik>
+      </header>
+    </div>
+  );
+};
+
+export default SearchBar;
